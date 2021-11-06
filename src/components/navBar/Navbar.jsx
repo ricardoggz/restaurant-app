94409d99d98ba25e6eb15c4403050e138cc04980
ricardoggz <ricardo_ggzicm@outlook.com>
2021-11-06 17:23:33 -0600

    NavBar finished

--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { RiMenu4Line } from "react-icons/ri";
 import "./style/navBar.css";
 import Logo from "./img/Logo.png";
 
@@ -6,12 +8,38 @@ const NavBar = () => {
   return (
     <header>
       <div className="flex-container nav_bar">
-        <img src={Logo} alt="Restaurant-App" className="nav_bar_logo"/>
+        <img src={Logo} alt="Restaurant-App" className="nav_bar_logo" />
         <nav>
+          <input type="checkbox" id="menu" />
+          <label htmlFor="menu" className="bar-btn">
+            <RiMenu4Line />
+          </label>
           <ul className="nav_bar_list">
-            <li>Inicio</li>
-            <li>Sobre nosotros</li>
-            <li>Contacto</li>
+            <li>
+              <Link to="#" className="link">
+                Inicio
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="link">
+                Información
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="link">
+                Comidas
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="link">
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="link">
+                Contacto
+              </Link>
+            </li>
           </ul>
         </nav>
       </div>
