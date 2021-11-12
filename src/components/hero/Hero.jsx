fb8a8e720941e684e2f9d50db9c9899fb5c81a4d
ricardoggz <ricardo_ggzicm@outlook.com>
2021-11-11 20:05:12 -0600

    hero modified

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,16 +2,10 @@ import React from "react";
 import { Button } from "../button/Button";
 import { Link } from "react-router-dom";
 import "./style/hero.css";
-import pizzadish from "./img/pizza-dish.jpg";
 
 function Hero() {
   return (
-    <section className="hero_container flex-container sections-container">
-      <div>
-        <figure>
-          <img src={pizzadish} alt="Un estómago vacío es un mal consejero" />
-        </figure>
-      </div>
+    <section className="hero_container">
       <div className="titles">
         <span>En esta cocina pasan cosas mágicas...</span>
         <p>"Goza inteligentemente de los placeres de la vida"</p>
